87a416cd4f9243d474a83141d724ba3edebbf22d
SergioPoves-daw <spovescamison.guadalupe@alumnado.fundacionloyola.net>
2026-01-22 15:07:03 +0100

    Tablas de fechas creadas

--- SQL_SQLSERVER.sql
+++ SQL_SQLSERVER.sql
@@ -37,3 +37,16 @@ CREATE TABLE charlas_ponentes (
 	FOREIGN KEY (idCharla) REFERENCES charlas(idCharla),	-- FK de charlas(idCharla) a charlas_ponentes(idCharla)
 	FOREIGN KEY (idPonente) REFERENCES ponentes(idPonente)	-- FK de ponentes(idPonente) a charlas_ponentes(idPonente)
 );
+
+-- Crear la tabla FECHAS y FECHAS CHARLAS (fecha y hora juntos)
+CREATE TABLE fechas (
+	fechahora smalldatetime PRIMARY KEY
+);
+
+CREATE TABLE fechas_charlas (
+	idCharla smallint NOT NULL,
+	fechahora smalldatetime NOT NULL,
+	PRIMARY KEY (idCharla, fechahora),
+	FOREIGN KEY (idCharla) REFERENCES charlas(idCharla),	-- FK de charlas(idCharla) a fechas_charlas(idCharla)
+	FOREIGN KEY (fechahora) REFERENCES fechas(fechahora)	-- FK de fechas(fechahora) a fechas_charlas(fechahora)
+);
